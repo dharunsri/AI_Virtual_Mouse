8379eedd46c3fb0b9065067967fd5f94f635550e
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-12-06 12:21:12 +0530

    AI Virtual Mouse
    Using Hand gestures mouse can be controlled. Developed by python language.

---
+++ AI Virtual Mouse.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import autopy as ap
+
+cam = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands    #Initializing mediapipe
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands = 1,
+                      min_detection_confidence=0.5,     #object of mediapipe with arguments for the hands module
+                      min_tracking_confidence=0.5)
+draw = mp.solutions.drawing_utils   # object to draw connections between each finger index
+wScr, hScr = ap.screen.size()   #Outputs the height and width of the screen (1920x1080)
+pX, pY = 0,0    # Previous x and y locations
+cX, cY = 0,0    # Current x and y locations
+
+def handTracking(ImgRGB):
+    lmList = []     # Default values if no landmark are tracked(empty list)
+
+    lmPositions = hands.process(ImgRGB) # object processing the video input
+    lmCheck = lmPositions.multi_hand_landmarks # Stores the out of the processing object(returns False on empty)
+    if lmCheck: #checks if landmarks are tracked
+        for hand in lmCheck:    # landmarks for each hand
+            for id, lm in enumerate(hand.landmark): # loops through the 21 indexes and outputs their landmark coordinates(x,y,z)
+                draw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)    #Draw each individual index on the hand with connections
+                h, w, c = img.shape # Height, width and channel on the image
+                centerX, centerY = int(lm.x*w), int(lm.y*h) # Converts the decimal coordinates relative to the image for each index
+                lmList.append([id, centerX, centerY]) # Adding index and its coordinates to a list
+
+
+    return lmList
+
+def fingers(landmarks):
+    fingerTips = [] # To store 4 sets of 1s or 0s
+    tipIds = [4,8,12,16,20] # Indexes for the tips of each finger
+
+    #check if thumb is up
+    if landmarks[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+        fingerTips.append(1)
+    else:
+        fingerTips.append(0)
+
+    #check if fingers are up except the thumb
+    for id in range(1,5):
+        if landmarks[tipIds[id]][2]<landmarks[tipIds[id]-3][2]:  #checks to see if the tip of the finger is higher than the joint
+            fingerTips.append(1)
+        else:
+            fingerTips.append(0)
+
+    return fingerTips
+
+while True:
+    check, img = cam.read() # Reads the frames from camera
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # changes the format of the frames from BGR to RGB
+    lmList = handTracking(imgRGB)
+
+    if len(lmList)!=0:
+        x1,y1 = lmList[8][1:]   # Gets index 8s x and y values (skips index value because it starts from 1)
+        x2,y2 = lmList[12][1:]  # Gets index 12s x and y values (skips index value because it starts from 1)
+        finger = fingers(lmList)    # Calling fingers function to check which fingers are up
+
+        if finger[1] == 1 and finger[2] == 0:   # checks to see if the pointing finger is up and thumb finger is down
+            x3 = np.interp(x1, (75, 650 - 75), (0, wScr))   # Converts the width of the window relative to the screen width
+            y3 = np.interp(y1, (75, 480 - 75), (0,hScr))    # Converts the height of the window relative to the screen height
+
+            cX = pX + (x3 - pX) / 7     # stores previous x locations to update current x location
+            cY = pY + (y3 - pY) / 7     # stores previous y locations to update current y location
+
+            ap.mouse.move(wScr-cX, cY)  # Function to move the mouse to the x3 and y3 values (wScr inverts the direction)
+            pX, pY = cX, cY     # stores the current x and y location as previous x and y location
+
+        if finger[1] == 0 and finger[0] == 1:   # checks to see if the pointer finger is down and thumb finger is up
+            ap.mouse.click()    #Left Click
+
+    cv2.imshow("AI Virtual Mouse", img)
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+            
